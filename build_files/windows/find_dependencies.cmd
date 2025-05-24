1a8fd32973597d5dba47c64fd07503d951953030
rPhase <25046850+rPhase@users.noreply.github.com>
2025-05-24 16:43:05 -0700

    Update find_dependencies.cmd
    fix

--- build_files/windows/find_dependencies.cmd
+++ build_files/windows/find_dependencies.cmd
@@ -7,19 +7,19 @@ REM For python, default on 310 but if that does not exist also check
 REM the 311, 312 and finally 39 folders to see if those are there, it checks
 REM this far ahead to ensure good lib folder compatibility in the future
 REM it falls back to 3.9 just incase it is a very old lib folder.
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\310\bin\python.exe
+set PYTHON=%BLENDER_DIR%\lib\win64_vc15\python\310\bin\python.exe
 if EXIST %PYTHON% (
 	goto detect_python_done
 )
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\311\bin\python.exe
+set PYTHON=%BLENDER_DIR%\lib\win64_vc15\python\311\bin\python.exe
 if EXIST %PYTHON% (
 	goto detect_python_done
 )
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\312\bin\python.exe
+set PYTHON=%BLENDER_DIR%\lib\win64_vc15\python\312\bin\python.exe
 if EXIST %PYTHON% (
 	goto detect_python_done
 )
-set PYTHON=%BLENDER_DIR%\..\lib\win64_vc15\python\39\bin\python.exe
+set PYTHON=%BLENDER_DIR%\lib\win64_vc15\python\39\bin\python.exe
 if EXIST %PYTHON% (
 	goto detect_python_done
 )
